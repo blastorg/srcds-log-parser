1c998568376ae7297af67833aad23c50650026fb
Fredrik Johansen <fredrik1206@gmail.com>
2023-10-30 11:54:04 +0100

    feat(log-parser): adds log parser with new updated steam id finder regex

---
+++ src/parsers/warmod.ts
@@ -0,0 +1,18 @@
+import { IBaseEvent, defineParser } from "./parser";
+import { concatPattern } from "../helpers";
+
+export type WarmodEventPayload = Record<string, unknown>;
+
+export type WarmodEvent = IBaseEvent<"warmod", WarmodEventPayload>;
+
+// eslint-disable-next-line max-len
+// [WarMod_BFG] {JSON}
+export const warmodParser = defineParser<WarmodEvent>({
+  type: "warmod",
+
+  patterns: [concatPattern`^\\[WarMod_BFG\\] (?<payload>.+)$`],
+
+  parse({ payload }) {
+    return JSON.parse(payload);
+  },
+});
